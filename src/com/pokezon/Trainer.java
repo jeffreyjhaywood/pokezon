f0e4d0dfebed7147a8fd5d6c258b7e4caf3c9002
Marcelo Nazario <ind7@protonmail.com>
2020-06-19 21:17:22 -0500

    Add currentPokezon variable, method. And resolve to comments.

--- src/com/pokezon/Trainer.java
+++ src/com/pokezon/Trainer.java
@@ -6,51 +6,26 @@ public class Trainer {
 
     // INSTANCE VARIABLES
     private String name;
-    Pokezon[] pokezonTeam ={new Pokezon(), new Pokezon(), new Pokezon()};
-    int numWins;
+    private Pokezon[] pokezonTeam;
+    private int numWins;
+    private Pokezon currentPokezon;
 
     // CONSTRUCTORS
 
     // Create Trainer after user input name
     public Trainer(String name) {
-        setName();
+        setName(name);
         //setRandomBattleType();
-        choosePokezon();
     }
 
-    // BUSINESS METHODS - NEEDS LIST OF POKEZONS AND MOVES
-    public void choosePokezon(){
-        // GET LIST OF POKEZONS
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        List<Pokezon> pokezonList = Arrays.asList();
-        // SAVE USER'S SELECTION
-        int pokezonSelection = scanner.nextInt();
-        // IF VALID POKEZON, ASSIGN TO USER INSTANCE TEAM
-        /*scanner.nextInt()*/
-        if (pokezonList.contains(pokezonSelection))
-            // Need to find out how to add new Pokezon to array
-            pokezonTeam = new Pokezon[0];
-        else
-            // something similar or loop until correct input
-            //selectAnotherPokezon();
-            return;
+    // BUSINESS METHODS
+    public void choosePokezon(int choice){
+        this.currentPokezon = pokezonTeam[choice];
     }
 
     //
-    public void chooseMove(Move name){
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        // GET LIST OF MOVES
-        List<Move> moveList = Arrays.asList();
-        // SAVE USER'S SELECTION
-        int moveSelection = scanner.nextInt();
-        // IF VALID MOVE, PERFORM MOVE
-        if (moveList.contains(moveSelection))
-            //executeMove();
-            return;
-        else
-            // something similar or loop until correct input
-            //executeAnotherMove();
-            return;
+    public void chooseMove(int choice){
+        currentPokezon.useMove(choice);
     }
 
     // GETTERS AND SETTERS
@@ -58,10 +33,8 @@ public class Trainer {
         return name;
     }
 
-    public void setName() {
-        System.out.println("What's your name?");
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        this.name = scanner.nextLine();
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getNumWins() {
