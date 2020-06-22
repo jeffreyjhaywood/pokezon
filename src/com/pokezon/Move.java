69ae7ca1cd989c284a155fbcb97118e659154723
justin lamb <justin.cory.lamb@gmail.com>
2020-06-22 07:54:42 -0700

    Added Blueprint

--- src/com/pokezon/Move.java
+++ src/com/pokezon/Move.java
@@ -1,11 +1,48 @@
 package com.pokezon;
 
+public class Move {
+    public static String name;
+    public static String type;
+    public static int BaseAttack;
+    public static int Mana;
+    public static int CurrentMana;
 
-    public class Move{
 
-        private String name;
 
+    public static String getName() {
+        return name;
     }
+
+    public static String getType() {
+        return type;
+    }
+
+    public static int getBaseAttack() {
+        return BaseAttack;
+    }
+
+    public static int getMana() {
+        return Mana;
+    }
+
+    public static int getCurrentMana() {
+        return CurrentMana;
+    }
+
+       private void (String name, String Type,int Mana, int BaseAttack, int CurrentMana){
+            Move.name = name;
+            Move.Mana = Mana;
+            Move.BaseAttack = BaseAttack;
+            Move.CurrentMana = CurrentMana;
+
+            System.out.println("This is:" + name);
+            System.out.println(name + "is a:" + type);
+            System.out.println("Attacking with:" + BaseAttack);
+       }
+
+
+}
+
 
 
 
