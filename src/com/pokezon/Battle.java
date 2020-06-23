42e1ed5d1a947acddd3da781f656e59b3b916eb4
Marcelo Nazario <ind7@protonmail.com>
2020-06-23 10:34:31 -0500

    Add boolean isBattleOver Add int numRounds

--- src/com/pokezon/Battle.java
+++ src/com/pokezon/Battle.java
@@ -4,14 +4,18 @@ public abstract class Battle {
     // BATTLE INSTANCE VARIABLES
     private Trainer player;
     private double xpForWin;
+    private boolean battleHappening;
+    private boolean isBattleOver = false;
+    private int numRounds;
     // BATTLE CONSTANTS
     public final double BASE_XP = 10.0;
 
     // BATTLE BUSINESS METHODS
-    public static Battle randomBattle(){
+    public static Battle randomBattle() {
         // Need to downcast in client to access subclass-only properties
         return (Math.random() < .5) ? new TrainerBattle() : new WildBattle();
     }
+
     public abstract void giveXpForWin();
 
     // BATTLE GETTERS AND SETTERS
@@ -29,6 +33,22 @@ public abstract class Battle {
 
     public void setXpForWin(double xpForWin) {
         this.xpForWin = xpForWin;
+    }
+
+    public boolean isBattleOver() {
+        return isBattleOver;
+    }
+
+    public void setBattleOver(boolean isBattleOver) {
+        this.isBattleOver = isBattleOver;
+    }
+
+    public int getNumRounds() {
+        return numRounds;
+    }
+
+    public void setNumRounds(int numRounds) {
+        this.numRounds = numRounds;
     }
 
     @Override
