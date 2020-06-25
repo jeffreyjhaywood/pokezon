2af725b9607768bed5371c1ad4842813c6365f48
Marcelo Nazario <ind7@protonmail.com>
2020-06-24 20:11:49 -0500

    Add loadingBar method and overload print to required user to press enter key after each dialogue I need to add 0 as second parameter to some SlowTyper lines on dialogue because not all lines requires user to press enter to show lines, like when listing Pokezons, Attacks, etc. I already implemented it on my local dialogue copy.

--- src/com/pokezon/util/SlowTyper.java
+++ src/com/pokezon/util/SlowTyper.java
@@ -1,12 +1,48 @@
 package com.pokezon.util;
 
+import java.util.Scanner;
+
 public final class SlowTyper {
+
     public static void print(String input){
         for(int i = 0; i < input.length(); i++) {
             System.out.print(input.charAt(i));
             try
             {
-                Thread.sleep(0);
+                Thread.sleep(30);
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Interrupted Exception");
+            }
+        }
+        System.out.print("\n");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+    }
+
+    public static void print(String input, int zeroForNoEnterWhenInputRequest){
+        for(int i = 0; i < input.length(); i++) {
+            System.out.print(input.charAt(i));
+            try
+            {
+                Thread.sleep(30);
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Interrupted Exception");
+            }
+        }
+        System.out.print("\n");
+    }
+
+    public static void loadingBar(){
+        String bar = "|||||||||||||||||||||||||||||||||||||||||||||:100%";
+        for(int i = 0; i < bar.length(); i++) {
+            System.out.print(bar.charAt(i));
+            try
+            {
+                Thread.sleep(115);
             }
             catch (InterruptedException ie)
             {
